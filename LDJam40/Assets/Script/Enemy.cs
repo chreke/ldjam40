6021f90976611e30f49f759c5da329d9dfd87f39
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2017-12-02 18:12:26 +0100

    Update FireTrap behaviour

---
+++ LDJam40/Assets/Script/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+				
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Character character = other.gameObject.GetComponent<Character>();
+		character.Kill();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- LDJam40/Assets/Script/FireTrap.cs
+++ LDJam40/Assets/Script/FireTrap.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireTrap : MonoBehaviour {
+	private BoxCollider2D boxCollider;
+	private SpriteRenderer spriteRenderer;
+	private const float burnTime = 1f;
+	private const float idleTime = 3f;
+	private bool isBurning = false;
+
+	void Awake() {
+		boxCollider = GetComponent<BoxCollider2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		Character character = other.gameObject.GetComponent<Character>();
-		character.Kill();
+	private IEnumerator Burn() {
+		isBurning = true;
+		spriteRenderer.enabled = true;
+		boxCollider.enabled = true;
+		yield return new WaitForSeconds(burnTime);
+		spriteRenderer.enabled = false;
+		boxCollider.enabled = false;
+		yield return new WaitForSeconds(idleTime);
+		isBurning = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isBurning) {
+			Burn();
+		}
 	}
 }
