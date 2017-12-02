d8ff034bb95b1029707920a404c4a08e666808c8
John Roos <johnroos@RunCarSnowPen.lan>
2017-12-02 11:45:29 +0100

    Add player movement

--- LDJam40/Assets/Script/PlayerController.cs
+++ LDJam40/Assets/Script/PlayerController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	float speed = 3.5f;
+	float jumpForce = 350f;
+
+	private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Move ();
+	}
+
+	void Move() {
+		float moveDirection = 0;
+		if (Input.GetAxisRaw ("Horizontal") != 0) {
+			moveDirection = Mathf.Sign (Input.GetAxis ("Horizontal"));
+		}
+		transform.position += Vector3.right * moveDirection * Time.deltaTime * speed;
 	}
 }
