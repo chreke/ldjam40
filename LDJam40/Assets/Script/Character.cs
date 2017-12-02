3e7996bd0b66d790992615c441aa3e75c4ce7934
John Roos <johnroos@RunCarSnowPen.lan>
2017-12-02 15:35:53 +0100

    Now moving with physics!

--- LDJam40/Assets/Script/Character.cs
+++ LDJam40/Assets/Script/Character.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Character : MonoBehaviour {
 
-	float moveSpeed = 3.5f;
-	float jumpForce = 350f;
+	public float maxSpeed = 3.5f;
+	public float moveSpeed = 2.5f;
+	public float dampAmount = 0.15f;
+	public float jumpForce = 350f;
 
 	private Rigidbody2D rb;
+	public bool isGrounded = true;
+
+	int numberOfRays = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +23,50 @@ public class Character : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CheckIfGrounded();
 	}
 
 	public void Move(int direction) {
-		transform.position += Vector3.right * direction * Time.deltaTime * moveSpeed;
+		//rb.velocity = new Vector2(direction * moveSpeed
+		//transform.position += Vector3.right * direction * Time.deltaTime * moveSpeed;
 	}
 
 	public void Jump() {
-		rb.AddForce (Vector2.up * jumpForce);
+		if(isGrounded) {
+			rb.AddForce(Vector2.up * jumpForce);
+		}
+	}
+
+	void CheckIfGrounded(){
+		isGrounded = false;
+		float raySpacing = transform.localScale.x / (numberOfRays - 1);
+		Vector3 bottomLeft = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y - transform.localScale.y / 2);
+
+		for(int i = 0; i < numberOfRays; i++){
+			Vector3 startCastFrom = new Vector3(bottomLeft.x + raySpacing * i, bottomLeft.y);
+			Vector3 endCastAt = new Vector3(bottomLeft.x + raySpacing * i, bottomLeft.y - 0.05f);
+
+			Debug.DrawLine(startCastFrom, endCastAt, Color.red);
+
+			if(Physics2D.Linecast(startCastFrom, endCastAt, 1 << LayerMask.NameToLayer("Ground"))){
+				isGrounded = true;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxisRaw("Horizontal");
+
+		if(Mathf.Abs(h) != 0) {
+			if(h * rb.velocity.x < maxSpeed) {
+				rb.AddForce(Vector2.right * h * moveSpeed);
+			}
+
+			if(Mathf.Abs(rb.velocity.x) > maxSpeed) {
+				rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+			}
+		} else {
+			rb.velocity = new Vector2(rb.velocity.x * (1-dampAmount), rb.velocity.y);
+		}
 	}
 }
--- LDJam40/Assets/Script/PlayerController.cs
+++ LDJam40/Assets/Script/PlayerController.cs
@@ -8,22 +8,22 @@ public class PlayerController : MonoBehaviour {
 	private Character player;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		player = GetComponent<Character> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		Move ();
 	}
 
 	void Move() {
 
-		if (Input.GetButtonDown ("Jump")) {
+		if (Input.GetButtonDown("Jump")) {
 			player.Jump();
 		}
 
-		if (Input.GetAxisRaw ("Horizontal") != 0) {
+		if (Input.GetAxisRaw("Horizontal") != 0) {
 			player.Move ((int)Mathf.Sign(Input.GetAxisRaw ("Horizontal")));
 		}
 
