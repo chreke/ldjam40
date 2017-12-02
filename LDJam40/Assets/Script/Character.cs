6e719e9b585733b7de42e7915bab3bb70b36b64a
John Roos <johnroos@RunCarSnowPen.lan>
2017-12-02 12:16:11 +0100

    Separate controls from character

---
+++ LDJam40/Assets/Script/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Character : MonoBehaviour {
+
+	float moveSpeed = 3.5f;
+	float jumpForce = 350f;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Move(int direction) {
+		transform.position += Vector3.right * direction * Time.deltaTime * moveSpeed;
+	}
+
+	public void Jump() {
+		rb.AddForce (Vector2.up * jumpForce);
+	}
+}
--- LDJam40/Assets/Script/PlayerController.cs
+++ LDJam40/Assets/Script/PlayerController.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Character))]
 public class PlayerController : MonoBehaviour {
 
-	float speed = 3.5f;
-	float jumpForce = 350f;
-
-	private Rigidbody2D rb;
+	private Character player;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();	
+		player = GetComponent<Character> ();
 	}
 	
 	// Update is called once per frame
@@ -20,13 +18,14 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Move() {
-		transform.position += Vector3.right * Input.GetAxisRaw ("Horizontal") * Time.deltaTime * speed;
-		if (Input.GetButtonDown ("Jump")) {
-			Jump ();
-		}		
-	}
 
-	void Jump() {
-		rb.AddForce (Vector2.up * jumpForce);
+		if (Input.GetButtonDown ("Jump")) {
+			player.Jump();
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") != 0) {
+			player.Move ((int)Mathf.Sign(Input.GetAxisRaw ("Horizontal")));
+		}
+
 	}
 }
